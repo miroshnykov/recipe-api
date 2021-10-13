fbf713a3f1d27c0ca7716b1579c0b034f0703126
miroshnykov <dmytro.miroshnykov@actionmediamtl.com>
2021-10-12 23:03:40 -0400

    some metrics offers size

--- src/crons/offersRecipe.ts
+++ src/crons/offersRecipe.ts
@@ -1,7 +1,7 @@
 import JSONStream from "JSONStream";
 import consola from "consola";
 import fileSystem from "fs";
-import {uploadOffersFileToS3Bucket} from "./offersRecipeSendToS3";
+import {checkSizeOfferFileFromS3Bucket, uploadOffersFileToS3Bucket} from "./offersRecipeSendToS3";
 import {compressFile, deleteFile} from "../utils";
 import {getOffers, getAggregatedOffers, getOfferCaps} from "../models/offersModel";
 import {setFileSizeOffers} from "./offersFileSize";
@@ -10,9 +10,9 @@ import {influxdb} from "../metrics";
 export const setOffersRecipe = async () => {
 
   try {
-    let offers:object|any = await getOffers()
+    let offers: object | any = await getOffers()
 
-    let offerFormat:any = []
+    let offerFormat: any = []
     for (const offer of offers) {
       if (offer.type === 'aggregated') {
         offer.offersAggregatedIds = await getAggregatedOffers(offer.offerId)
