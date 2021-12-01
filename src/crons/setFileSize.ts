5521beae3e9bbd8561601a12ad949a240e5ade2e
miroshnykov <dmytro.miroshnykov@actionmediamtl.com>
2021-12-01 14:26:41 -0500

    renamed some variables for redis

--- src/crons/setFileSize.ts
+++ src/crons/setFileSize.ts
@@ -6,7 +6,7 @@ import {influxdb} from "../metrics";
 export const setFileSize = async (type: IRecipeType, sizeDB: number) => {
   try {
     consola.info(`set ${type}Size:${sizeDB} to Redis`)
-    await redis.set(`${type}Size`, sizeDB!)
+    await redis.set(`${type}SizeRecipe`, sizeDB!)
   } catch (e) {
     consola.error(`setFileSize${type}:`, e)
     influxdb(500, `set_file_size_redis_${type}_error`)
