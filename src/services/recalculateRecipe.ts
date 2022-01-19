1d7e55be954fdda16adc956f90c2c7372144801d
miroshnykov <dmytro.miroshnykov@actionmediamtl.com>
2022-01-19 16:10:41 -0500

    fix some syntax error

--- src/services/recalculateRecipe.ts
+++ src/services/recalculateRecipe.ts
@@ -16,8 +16,7 @@ const offerUpdateOrCreate = async (messageBody: ISqsMessage): Promise<ISqsMessag
   const messageResponse = [];
   switch (offer.status) {
     case IOfferStatus.INACTIVE:
-    case IOfferStatus.DRAFT:
-      // eslint-disable-next-line no-case-declarations
+    case IOfferStatus.DRAFT: {
       const generateOfferBodyForDelete: ISqsMessage = {
         comments: 'offer status inactive or draft, lets delete from recipe',
         type: ISqsMessageType.OFFER,
@@ -31,13 +30,12 @@ const offerUpdateOrCreate = async (messageBody: ISqsMessage): Promise<ISqsMessag
       influxdb(200, `sqs_offer_inactive_${projectName}`);
       messageResponse.push(generateOfferBodyForDelete);
       break;
+    }
     case IOfferStatus.PUBLIC:
     case IOfferStatus.PENDING:
     case IOfferStatus.APPLY_TO_RUN:
-    case IOfferStatus.PRIVATE:
-      // eslint-disable-next-line no-case-declarations
+    case IOfferStatus.PRIVATE: {
       const reCalculatedOffer: IOffer | any = await reCalculateOffer(offer);
-      // eslint-disable-next-line no-case-declarations
       const generateOfferBody: ISqsMessage = {
         comments: messageBody.comments,
         type: ISqsMessageType.OFFER,
@@ -51,14 +49,11 @@ const offerUpdateOrCreate = async (messageBody: ISqsMessage): Promise<ISqsMessag
       // messages.push(JSON.parse(generateOfferBody))
       influxdb(200, `sqs_offer_update_or_create_${projectName}`);
       messageResponse.push(generateOfferBody);
-
       // PH-328
-      // eslint-disable-next-line no-case-declarations
       const findAgg = await findAggregatedOffer(messageBody.id);
       if (findAgg.sfl_offer_aggregated_id) {
-        // eslint-disable-next-line @typescript-eslint/no-shadow
-        const offer: IOffer = await getOffer(findAgg.sfl_offer_aggregated_id);
-        const reCalculateAggregatedOffer: IOffer | any = await reCalculateOffer(offer);
+        const offerFoAggr: IOffer = await getOffer(findAgg.sfl_offer_aggregated_id);
+        const reCalculateAggregatedOffer: IOffer | any = await reCalculateOffer(offerFoAggr);
         const aggrOfferBody: ISqsMessage = {
           comments: 'recalculate aggregated offer',
           type: ISqsMessageType.OFFER,
@@ -71,8 +66,8 @@ const offerUpdateOrCreate = async (messageBody: ISqsMessage): Promise<ISqsMessag
         influxdb(200, `sqs_offer_update_aggregated_${projectName}`);
         messageResponse.push(aggrOfferBody);
       }
-
       break;
+    }
     default:
       consola.info('offerReCalculate status not defined');
       influxdb(500, `sqs_offer_status_not_defined_${projectName}`);
@@ -80,47 +75,20 @@ const offerUpdateOrCreate = async (messageBody: ISqsMessage): Promise<ISqsMessag
   return messageResponse.flat();
 };
 
-export const recalculateRecipe = async (message: any): Promise<ISqsMessage[]> => {
-  const responseEntity = [];
-  try {
-    const messageBody = JSON.parse(message.Body!);
-
-    switch (messageBody.type) {
-      case ISqsMessageType.OFFER:
-        // eslint-disable-next-line @typescript-eslint/no-use-before-define,no-case-declarations
-        const resOffer = await offerReCalculate(messageBody);
-        responseEntity.push(resOffer);
-        break;
-      case ISqsMessageType.CAMPAIGN:
-        // eslint-disable-next-line @typescript-eslint/no-use-before-define,no-case-declarations
-        const response = await campaignReCalculate(messageBody);
-        responseEntity.push(response);
-        break;
-      default:
-        consola.info('recalculateRecipe sqs message type not defined');
-        influxdb(500, 'sqs_message_type_not_defined');
-    }
-
-    await deleteMessage(message.ReceiptHandle!);
-    return responseEntity.flat();
-  } catch (e) {
-    return [];
-  }
-};
-
 const offerReCalculate = async (messageBody: ISqsMessage): Promise<ISqsMessage[]> => {
   const messageResponse = [];
 
   switch (messageBody.action) {
-    case ISqsMessageAction.UPDATE_OR_CREATE:
-      // eslint-disable-next-line no-case-declarations
+    case ISqsMessageAction.UPDATE_OR_CREATE: {
       const offerResponse = await offerUpdateOrCreate(messageBody);
       messageResponse.push(offerResponse);
       break;
-    case ISqsMessageAction.DELETE:
+    }
+    case ISqsMessageAction.DELETE: {
       influxdb(200, 'sqs_offer_delete');
       messageResponse.push(messageBody);
       break;
+    }
     default:
       messageResponse.push(messageBody);
   }
@@ -131,12 +99,9 @@ const campaignReCalculate = async (messageBody: ISqsMessage): Promise<ISqsMessag
   const messageResponse = [];
   const projectName = messageBody?.project || '';
   switch (messageBody.action) {
-    case ISqsMessageAction.UPDATE_OR_CREATE:
-      // eslint-disable-next-line no-case-declarations
+    case ISqsMessageAction.UPDATE_OR_CREATE: {
       const campaignInfo: ICampaign = await getCampaign(messageBody.id);
-      // eslint-disable-next-line no-case-declarations
       const reCalcCampaign: ICampaign | any[] = await reCalculateCampaignCaps(campaignInfo.campaignId);
-      // eslint-disable-next-line no-case-declarations
       const generateCampaignBody: ISqsMessage = {
         comments: messageBody.comments,
         type: ISqsMessageType.CAMPAIGN,
@@ -149,12 +114,44 @@ const campaignReCalculate = async (messageBody: ISqsMessage): Promise<ISqsMessag
       influxdb(200, `sqs_campaign_update_or_create_${projectName}`);
       messageResponse.push(generateCampaignBody);
       break;
-    case ISqsMessageAction.DELETE:
+    }
+
+    case ISqsMessageAction.DELETE: {
       influxdb(200, 'sqs_campaign_delete');
       messageResponse.push(messageBody);
       break;
+    }
+
     default:
       messageResponse.push(messageBody);
   }
   return messageResponse.flat();
+};
+
+export const recalculateRecipe = async (message: any): Promise<ISqsMessage[]> => {
+  const responseEntity = [];
+  try {
+    const messageBody = JSON.parse(message.Body!);
+
+    switch (messageBody.type) {
+      case ISqsMessageType.OFFER: {
+        const resOffer = await offerReCalculate(messageBody);
+        responseEntity.push(resOffer);
+        break;
+      }
+      case ISqsMessageType.CAMPAIGN: {
+        const response = await campaignReCalculate(messageBody);
+        responseEntity.push(response);
+        break;
+      }
+      default:
+        consola.info('recalculateRecipe sqs message type not defined');
+        influxdb(500, 'sqs_message_type_not_defined');
+    }
+
+    await deleteMessage(message.ReceiptHandle!);
+    return responseEntity.flat();
+  } catch (e) {
+    return [];
+  }
 };
